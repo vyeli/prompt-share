b1fda8f9cfefbeb9b073574d323eb2076ac9b24f
VALENTIN YE LI <yeliv9527@gmail.com>
2023-06-30 20:42:17 -0300

    add navbar component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,31 @@
 import "@/styles/global.css"
 
+import Nav from "@/components/Nav"
+import Provider from "@/components/Provider"
+
 export const metadata = {
-    title: "PromtShare",
-    description: "PromtShare is a platform to Discover & Share AI Promts"
+    title: "PromptShare",
+    description: "PromptShare is a platform to Discover & Share AI Promts"
 }
 
 
-const Rootlayout = ({children}) => {
-  return (
-    <html lang="en">
-        <body>
-            <div className="main">
-                <div className="gradient"/>
-            </div>
+const Rootlayout = ({ children }) => {
+    return (
+        <html lang="en">
+            <body>
+                <Provider>
+                    <div className="main">
+                        <div className="gradient" />
+                    </div>
 
-            <main className="app">
-                {children}
-            </main>
-        </body>
-    </html>
-  )
+                    <main className="app">
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
+            </body>
+        </html>
+    )
 }
 
 export default Rootlayout;
---
+++ components/Nav.jsx
@@ -0,0 +1,138 @@
+"use client"
+
+import Link from 'next/link' // Link component from Next.js to link to other pages in the app
+import Image from 'next/image' // Image component from Next.js to optimize images
+import { useState, useEffect } from 'react'
+import {
+  signIn, signOut, useSession,
+  getProviders
+} from 'next-auth/react' // NextAuth.js functions to sign in and out
+import { set } from 'mongoose'
+
+
+const Nav = () => {
+  const isUserLoggedIn = true
+
+  const [providers, setProviders] = useState(null)
+  const [toggleDropdown, setToggleDropdown] = useState(false)
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders()
+
+      setProviders(response)
+    }
+
+    setProviders()
+  }, [])
+
+
+  return (
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href="/" className='flex gap-2 flex-center'>
+        <Image src="/assets/images/logo.svg" width={30} height={30} className='object-contain'
+          alt="Promtshare Logo" />
+        <p className='logo_text'>PromptShare</p>
+      </Link>
+      {/* Desktop navigation */}
+      {/* This div in small devices will be flex, in other cases will be hidden */}
+      <div className='sm:flex hidden'>
+        {isUserLoggedIn ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href="/create-prompt" className='black_btn'>
+              Create Prompt
+            </Link>
+
+            <button type='button' onClick={signOut}
+              className='outline_btn'>
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                alt='PromptShare Logo'
+                width={30}
+                height={30}
+                className='object-contain'
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers && Object.values(providers).map((provider) => (
+              <button
+                type='button'
+                key={provider.name}
+                onClick={() => signIn(provider.id)}
+                className='black_btn'
+              >
+                Sign in with {provider.name}
+              </button>
+            )
+            )}
+          </>
+        )}
+      </div>
+
+      {/* Mobile navigation */}
+      <div className='sm:hidden flex relative'>
+        {isUserLoggedIn ? (
+          <div className='flex'>
+            <Image
+              src="/assets/images/logo.svg"
+              alt='profile'
+              width={37}
+              height={37}
+              className='rounded-full'
+              onClick={() => setToggleDropdown(prev => !prev)}
+            />
+            {toggleDropdown && (
+              <div className='dropdown'>
+                <Link
+                  href="/profile"
+                  className='dropdown_link'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className='dropdown_link'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type='button'
+                  onClick={() => {
+                    setToggleDropdown(false)
+                    signOut()
+                  }}
+                  className='mt-5 w-full black_btn'>
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers && Object.values(providers).map((provider) => (
+              <button
+                type='button'
+                key={provider.name}
+                onClick={() => signIn(provider.id)}
+                className='black_btn'
+              >
+                Sign in with {provider.name}
+              </button>
+            )
+            )}
+          </>
+        )}
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
